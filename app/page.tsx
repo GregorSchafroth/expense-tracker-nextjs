057550d14d53f756b71fc04038cbfa50189cc394
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-08-16 11:12:53 +0200

    add transaction action

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Guest from "@/components/Guest"
+import { currentUser } from "@clerk/nextjs/server"
+import AddTransaction from "@/components/AddTransaction";
 
-const page = () => {
+const page = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />
+  }
+
   return (
-    <h1>Expense Tracker</h1>
+    <>
+    <h2>Welcome, {user.firstName}</h2>
+    <AddTransaction />
+    </>
   )
 }
 
